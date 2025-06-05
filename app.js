5c386f9beedffa0ed364cd955920d5f0d0a2fb3e
jalalkhai <jalalkhaizaran@gmail.com>
2025-06-05 11:51:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// ======= عناصر DOM =======
+const tabs = document.querySelectorAll("nav .tab-btn");
+const tabContents = document.querySelectorAll(".tab-content");
+
+// المخزون
+const addItemForm = document.getElementById("add-item-form");
+const itemsList = document.getElementById("items-list");
+
+// الحسابات
+const addAccountForm = document.getElementById("add-account-form");
+const accountsList = document.getElementById("accounts-list");
+const addTransactionForm = document.getElementById("add-transaction-form");
+const transactionAccount = document.get
